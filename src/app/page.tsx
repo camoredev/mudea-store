4bb3d710e6e1d247d457210710ed75608e02aeed
Caio Moraes <camoredev@gmail.com>
2023-10-26 02:19:58 -0300

    feat: add auth handler to header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { useSession } from 'next-auth/react';
 import Header from '../components/ui/Header'
 
 export default function Home() {
+  const { data } = useSession()
+
   return (
     <>
       <Header />
+      <h1>{data?.user?.name}</h1>
     </>
   );
 }
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,30 @@
+'use client'
+
 import React from 'react'
-import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCart } from 'lucide-react'
+import { HomeIcon, ListOrderedIcon, LogInIcon, LogOutIcon, MenuIcon, PercentIcon, ShoppingCart } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
+import { signIn, signOut,  useSession } from 'next-auth/react'
+
+//  criar um arquivo index para fazer exportação unica de component
 import { Card, CardContent } from './card'
 import { Button } from './button'
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from './sheet'
+import { Avatar } from './avatar'
+import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
+import { Separator } from './separator'
 
-export default function header() {
+export default function Header() {
+  
+  const {status, data} = useSession()
+
+
+  async function handleLoginCLick() {
+    await signIn()
+  }
+
+  async function handleLogOutCLick() {
+    await signOut()
+  }
+
   return (
     <Card className='flex justify-between p-[1.87rem]'>
       <CardContent className='flex justify-between items-center w-full'>
@@ -19,10 +39,33 @@ export default function header() {
             <SheetHeader className='flex text-center text-lg font-semibold'>
               Menu
             </SheetHeader>
-            <Button variant='outline' className='mt-8 w-full justify-start gap-2'>
+            {status === 'authenticated' &&
+            <div className='flex gap-2 justify-center items-center mt-4'>
+              <Avatar>
+                <AvatarFallback>
+                  user
+                </AvatarFallback>
+                <AvatarImage src={`${data?.user?.image }`}/>
+              </Avatar>
+              <div  className='flex flex-col gap-1'>
+                <p>{data?.user?.name }</p>
+                <span className='text-sm opacity-75'>Boas compras!!!</span>
+              </div>
+              <Button onClick={ handleLogOutCLick } variant='outline' className='flex ml-16 h-full justify-center items-center gap-2'>
+                <LogOutIcon/>
+                Sair
+              </Button>  
+            </div>
+            }
+
+            <Separator className='my-4'/>
+            {status === 'unauthenticated' &&
+            <Button onClick={ handleLoginCLick } variant='outline' className='mt-8 w-full justify-start gap-2'>
               <LogInIcon/>
               Fazer login
-            </Button>
+            </Button>            
+            }
+            
             <div className=' flex flex-col mt-4 gap-1'>
             <Button variant='ghost' className='w-full justify-start gap-2'>
                 <HomeIcon size={16}/>
