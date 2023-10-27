b3997e39f5053b716d8f5e32149eb6a8455a373e
Caio Moraes <camoredev@gmail.com>
2023-10-27 00:15:23 -0300

    chore: add image and header component in page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/providers/auth'
+import Header from '../components/ui/Header'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
         <body className={inter.className}>
           <AuthProvider>
+            <Header />
             {children}
           </AuthProvider>
         </body>
